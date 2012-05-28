a6d0be1c778f6132a0977483acef5ca52f7ba9c4
cgeyer <cgeyer@09a2bdc0-3751-4de4-99d9-04f024db765c>
2012-05-28 12:49:39 +0000

    - Finished first version of mpBuffer => needs testing

--- include/muParser/mpBuffer.h
+++ include/muParser/mpBuffer.h
@@ -45,8 +45,6 @@ protected:
   size_type _size;
   /** Limit maximum number of elements a buffer might contain. */
   static const size_type _max_size = (1<<25);
-  /** Save number of actually reserved memory. Must be greater or equal to _size. */
-  size_type _capacity;
 
   /** Allocator of current element type. */
   allocator_type _allocator;
@@ -61,17 +59,20 @@ protected:
   /** In case of a ring buffer, this is the index to the last valid element. */
   size_type _last;
 
+  /**  */
+  size_type _current_idx;
+
   /**
-   * @brief Releases all elements by calling the destructor and deallocating
-   *        all needed memory.
+   * @brief Releases all elements by calling the destructor of the saved elements
+   *        and deallocating all needed memory.
    */
   virtual void clean()
   {
-    for (size_type i = 0; i < _capacity; ++i)
+    for (size_type i = 0; i < _size; ++i)
     {
       _allocator.destroy(&_fields[i]);
     }
-    _allocator.deallocate(_fields, _capacity);
+    _allocator.deallocate(_fields, _size);
   }
 
 public:
@@ -82,28 +83,30 @@ public:
 
   explicit mpBuffer(const allocator_type& a = allocator_type() ) :
     _size(0),
-    _capacity(0),
+    //_capacity(0),
     _allocator(a),
     _fields(0),
     _is_ringbuffer(false),
     _first(0),
-    _last(0)
+    _last(0),
+    _current_idx(0)
   {}
 
   explicit mpBuffer(size_type n,
-                    const value_type& v = value_type(),
                     bool rb = false,
+                    const value_type& v = value_type(),
                     const allocator_type& a = allocator_type()) :
     _size(n),
-    _capacity(n + 10),
+    //_capacity(n + 10),
     _allocator(a),
-    _fields(_allocator.allocate(_capacity)),
+    _fields(_allocator.allocate(_size)),
     _is_ringbuffer(rb),
     _first(0),
-    _last(n)
+    _last(0),
+    _current_idx(0)
   {
     // construct objects
-    for (size_type i = 0; i < _capacity; ++i)
+    for (size_type i = 0; i < _size; ++i)
     {
       _allocator.construct(&_fields[i], v);
     }
@@ -114,15 +117,16 @@ public:
    */
   mpBuffer(const mpBuffer<value_type, allocator_type>& buf) :
     _size(buf._size),
-    _capacity(buf._capacity),
+    //_capacity(buf._capacity),
     _allocator(buf._allocator),
-    _fields(_allocator.allocate(_capacity)),
+    _fields(_allocator.allocate(_size)),
     _is_ringbuffer(buf._is_ringbuffer),
     _first(buf._first),
-    _last(buf._last)
+    _last(buf._last),
+    _current_idx(buf._current_idx)
   {
     // copy all elements
-    for (size_type i = 0; i < _capacity; ++i)
+    for (size_type i = 0; i < _size; ++i)
     {
       _allocator.construct(&(_fields[i]), buf._fields[i]);
     }
@@ -133,14 +137,15 @@ public:
   {
     clean();
     _size = buf._size;
-    _capacity = buf._capacity;
+    //_capacity = buf._capacity;
     _allocator = buf._allocator;
-    _fields = _allocator.allocate(_capacity);
+    _fields = _allocator.allocate(_size);
     _is_ringbuffer = buf._is_ringbuffer;
     _first = buf._first;
     _last = buf._last;
+    _current_idx = buf._current_idx;
     // copy all elements
-    for (size_type i = 0; i < _capacity; ++i)
+    for (size_type i = 0; i < _size; ++i)
     {
       _allocator.construct(&(_fields[i]), buf._fields[i]);
     }
@@ -170,24 +175,73 @@ public:
     return _max_size;
   }
 
-  void virtual resize(size_type sz, _Type c = _Type() )
+  void virtual resize(size_type sz, value_type c = value_type() )
   {
-    // TODO
+    pointer newFields = _allocator.allocate(sz);
+
+    // if we do not have a ring buffer, just copy the first elements to
+    // the newly allocated array
+    if (!_is_ringbuffer)
+    {
+      // number of fields to copy is minimum of current size and new size
+      size_type copy_fields = (sz > _size) ? _size : sz;
+      // copy all existing fields
+      for (size_type i = 0; i < copy_fields; ++i)
+      {
+        _allocator.construct(&(newFields[i]), _fields[i]);
+      }
+
+      // initialize all new elements if any
+      for (size_type i = copy_fields; i < sz; ++i)
+      {
+        _allocator.construct(&(newFields[i]), c);
+      }
+      _last = _size - 1;
+    }
+    else
+    // if we have a ring buffer, copy all elements from first to last
+    {
+      // calculate number of elements to copy
+      size_type copy_fields = (sz > _size) ?
+          ((_first - _last + 1 + _size) % _size) : (sz);
+      size_type bufPointer = (sz > _size) ? _last : ((_first - sz + _size) % _size);
+      for (size_type i = 0; i < copy_fields; ++i)
+      {
+        _allocator.construct(&(newFields[i]), _fields[bufPointer]);
+        bufPointer = (bufPointer + 1) % _size;
+      }
+
+      // initialize all new elements if any
+      for (size_type i = copy_fields; i < sz; ++i)
+      {
+        _allocator.construct(&(newFields[i]), c);
+      }
+
+      // set new element pointers for first and last
+      _first = (sz > _size) ? (copy_fields) : (sz);
+      _last = (_last > sz) ? (sz - 1) : 0;
+
+    }
+
+    // deallocate existing memory
+    clean();
+
+    // set new fields
+    _fields = newFields;
+    // set new size
+    _size = sz;
+
   }
 
   inline virtual size_type capacity() const
   {
-    return _capacity;
+    //return _capacity;
+    return _size;
   }
 
   inline virtual bool empty() const
   {
     return (_size == 0);
-  }
-
-  virtual void reserve(size_type n)
-  {
-    // TODO
   }
 
   /* ======================= */
@@ -200,7 +254,20 @@ public:
       throw std::out_of_range("mpBuffer has not been accessed properly");
 
     reference retVal = (_is_ringbuffer) ?
-        _fields[n % _capacity] : _fields[n];
+        _fields[n % _size] : _fields[n];
+
+    // save the lowest index of the current array access:
+    // if the currently used buffer size is less than the
+    // difference between current index and
+    if (((_first - _last + _size + 1) % _size) > (_current_idx - n))
+    {
+      // if we cannot increase the number of used elements, throw
+      // an out of range exception
+      if ((_current_idx - n) > _size)
+        throw std::out_of_range("Ring buffer access to element which is currently in use");
+
+      _last = n % _size;
+    }
 
     return retVal;
   }
@@ -211,7 +278,10 @@ public:
       throw std::out_of_range("mpBuffer has not been accessed properly");
 
     const_reference retVal = (_is_ringbuffer) ?
-        _fields[n % _capacity] : _fields[n];
+        _fields[n % _size] : _fields[n];
+
+    // as we cannot change the size of a constant buffer, we do
+    // not need to track the size of actually accessed elements
 
     return retVal;
   }
@@ -229,14 +299,14 @@ public:
   inline virtual reference front()
   {
     reference retVal = (_is_ringbuffer) ?
-        _fields[_first % _capacity] : _fields[0];
+        _fields[_first % _size] : _fields[0];
     return retVal;
   }
 
   inline virtual const_reference front() const
   {
     const_reference retVal = (_is_ringbuffer) ?
-        _fields[_first % _capacity] : _fields[0];
+        _fields[_first % _size] : _fields[0];
 
     return retVal;
   }
@@ -244,14 +314,14 @@ public:
   inline virtual reference back()
   {
     reference retVal = (_is_ringbuffer) ?
-        _fields[_last % _capacity] : _fields[_size];
+        _fields[_last % _size] : _fields[_size];
     return retVal;
   }
 
   inline virtual const_reference back() const
   {
     const_reference retVal = (_is_ringbuffer) ?
-        _fields[_last % _capacity] : _fields[_size];
+        _fields[_last % _size] : _fields[_size];
 
     return retVal;
   }
@@ -259,6 +329,36 @@ public:
   inline virtual allocator_type get_allocator() const
   {
     return _allocator;
+  }
+
+  /* ================================= */
+  /* public extensions for ring buffer */
+  /* ================================= */
+
+  /**
+   * @brief Sets the current index of the ring buffer to the given
+   *        value and adapts internal pointers to the first and
+   *        last element of the ring buffer.
+   */
+  inline virtual void setCurrentIdx(size_type n)
+  {
+    // the first element points to the given index modulo available buffer size
+    _first = n % _size;
+    // the last index is calculated based on the previous values of last and
+    // current_idx.
+    _last = (_last + (n - _current_idx) + _size) % _size;
+    // sets the current index to the given index.
+    _current_idx = n;
+  }
+
+  /**
+   * @brief Returns the number of accessed elements of the
+   *        ring buffer which is equivalent to the actually
+   *        used buffer size.
+   */
+  inline virtual size_type getUsedBufferSize(size_type n)
+  {
+    return ((_first - _last + 1 + _size) % _size);
   }
 
 
