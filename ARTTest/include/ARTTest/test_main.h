77f733d1971b1ecd3035f0149709d364aab25fa7
cgeyer <cgeyer@09a2bdc0-3751-4de4-99d9-04f024db765c>
2016-05-30 20:38:17 +0000

    Added gmock/gtest to ARTINTERFACEDEBUG build target
    - Adapted CMakeLists.txt in order to build target properly
    - Added gmock and gtest use cases to test_main.cpp
    - Adapted test_main.cpp to reflect new file structure

---
+++ ARTTest/include/ARTTest/test_main.h
@@ -0,0 +1,66 @@
+#ifndef TEST_MAIN_H
+
+#define TEST_MAIN_H
+
+#ifndef ARTinterfaceTest //Test the ARTinterface functions
+#  error "This header file should not be included for any release code."
+#else
+
+
+#if defined (_MSC_VER) && _MSC_VER <= 1200
+	#include "common_vs6.hpp"
+#else
+	#include "common.hpp"
+#endif
+#ifdef WIN32
+#include "windows.h" //just for setting color on the windows console... remove or replace when working on a different system
+#else
+// cbg: no SetConsoleTextAttribute() function on linux systems
+#define SetConsoleTextAttribute(a,b)
+// cbg: workaround for unix time measurements
+#define GetTickCount() 0
+#endif
+
+#include "ART.h"
+#include "ARTlink.h"
+#include "Interface.h"
+
+#include "gmock.h"
+#include "gtest.h"
+
+
+/*
+ * Definitions for mock class of ARTvariant
+ */
+
+class MockARTvariant : public ARTvariant
+
+{
+public:
+
+  MOCK_METHOD2(SetVal, void(const int i, const int ind));
+  MOCK_METHOD2(SetVal, void(const double d, const int ind));
+  MOCK_METHOD2(SetVal, void(const float f, const int ind));
+  MOCK_METHOD2(SetVal, void(std::complex<double>, const int ind));
+  MOCK_METHOD3(SetVal, void(const double re, const double im, const int ind));
+  MOCK_METHOD1(SetVal, void(const char* s));
+  MOCK_METHOD2(SetVal, void(const char* s, int ind));
+
+  MOCK_METHOD2(SetType, void(T_ART_Type t, int l));
+
+  MOCK_METHOD0(GetTypeString, std::string());
+
+  MOCK_METHOD1(IsEqual, bool(ARTvariant* other));
+  MOCK_METHOD1(IsEqual, bool(const char* s));
+
+  MOCK_METHOD1(SetArrayLength, void(int l));
+
+  MOCK_METHOD0(clone, ARTvariant*(void));
+
+};
+
+
+
+#endif /*ARTinterfaceTest*/
+
+#endif /* TEST_MAIN_H */
