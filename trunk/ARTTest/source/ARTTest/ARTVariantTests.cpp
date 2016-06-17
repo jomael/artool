e3493fc90a5ae2799ed4fcfa54c859d5f803242d
cgeyer <cgeyer@09a2bdc0-3751-4de4-99d9-04f024db765c>
2016-06-17 06:07:58 +0000

    Adding new test files
    - Split the current test files into several source files
    - Added different source test files for integration and unit tests

---
+++ trunk/ARTTest/source/ARTTest/ARTVariantTests.cpp
@@ -0,0 +1,39 @@
+/*
+ * ARTVariantTests.cpp
+ *
+ *  Created on: 13 Jun 2016
+ *      Author: clgeyer
+ */
+
+
+#include "ARTTest/ARTVariantTests.h"
+
+using ::testing::AtLeast;
+using ::testing::Return;
+
+// This test should pass
+TEST(ARTVariantChecks, SetVal1)
+{
+  MockARTvariant variable;
+
+  EXPECT_CALL(variable, SetVal(testing::Matcher<int>(2), testing::Matcher<int>(0)));
+  EXPECT_CALL(variable, IsEqual(static_cast<ARTvariant*>(0)));
+
+  // should pass
+  variable.SetVal(2,0);
+  variable.IsEqual(static_cast<ARTvariant*>(0));
+}
+
+TEST(ARTCellChecks, CellChecks1)
+{
+  ARTcell cell("OldName", "Short Description", "Long Description",
+                   "Helpfile Name");
+
+  EXPECT_EQ("OldName", cell.GetName());
+
+  cell.SetName("NewName");
+
+  EXPECT_EQ("NewName", cell.GetName());
+
+}
+
